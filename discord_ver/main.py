af4f17537479ec9bb559751dfe1825602075050d
09iue <iueedits09@gmail.com>
2025-05-28 09:35:59 +0700

    Repair main.py

--- discord_ver/main.py
+++ discord_ver/main.py
@@ -1,12 +1,16 @@
 import discord
+import sys
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
 
 # Configuration
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(BASEDIR))
 load_dotenv()
 dc_token = os.getenv("token")
 CHANNEL_ID = [1364460757084930101]
+
 
 class SurvyApp(commands.Bot):
     def __init__(self):
@@ -21,10 +25,11 @@ class SurvyApp(commands.Bot):
     
     async def setup_hook(self):
         # Load Cogs
-        for filename in os.listdir('./cogs'):
+        cogsdir = os.path.join(BASEDIR, 'cogs')
+        for filename in os.listdir(cogsdir):
             if filename.endswith('.py') and not filename.startswith('_'):
                 await self.load_extension(f'cogs.{filename[:-3]}')
-                print(f'Cog named {filename} loaded!')
+                print(f'{filename} loaded!')
 
         # Sync a Certain Location
         guild = discord.Object(id=int(os.getenv("OSBC")))
